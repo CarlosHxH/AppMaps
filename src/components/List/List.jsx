896f23aaabe449d2d314c69eb1679e04fbadc492
Carlos Alves <110472397+CarlosHxH@users.noreply.github.com>
2023-04-05 00:15:33 -0300

    loader...

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -14,7 +14,7 @@ const List = ({ places, type, setType, rating, setRating, childClicked, isLoadin
 
   return (
     <div className={classes.container}>
-      <Typography variant="h4">Food & Dining around you</Typography>
+      <Typography variant="h4">Lugares</Typography>
       {isLoading ? (
         <div className={classes.loading}>
           <CircularProgress size="5rem" />
@@ -22,20 +22,20 @@ const List = ({ places, type, setType, rating, setRating, childClicked, isLoadin
       ) : (
         <>
           <FormControl className={classes.formControl}>
-            <InputLabel id="type">Type</InputLabel>
+            <InputLabel id="type">Tipo</InputLabel>
             <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
-              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="restaurants">Restaurantes</MenuItem>
               <MenuItem value="hotels">Hotels</MenuItem>
-              <MenuItem value="attractions">Attractions</MenuItem>
+              <MenuItem value="attractions">Atrações</MenuItem>
             </Select>
           </FormControl>
           <FormControl className={classes.formControl}>
-            <InputLabel id="rating">Rating</InputLabel>
+            <InputLabel id="rating">Avaliações</InputLabel>
             <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
-              <MenuItem value="">All</MenuItem>
-              <MenuItem value="3">Above 3.0</MenuItem>
-              <MenuItem value="4">Above 4.0</MenuItem>
-              <MenuItem value="4.5">Above 4.5</MenuItem>
+              <MenuItem value="">Todos</MenuItem>
+              <MenuItem value="3">Acima 3.0</MenuItem>
+              <MenuItem value="4">Acima 4.0</MenuItem>
+              <MenuItem value="4.5">Acima 4.5</MenuItem>
             </Select>
           </FormControl>
           <Grid container spacing={3} className={classes.list}>
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -24,7 +24,7 @@ const PlaceDetails = ({ place, selected, refProp }) => {
           <Typography component="legend">{place.num_reviews} review{place.num_reviews > 1 && 's'}</Typography>
         </Box>
         <Box display="flex" justifyContent="space-between">
-          <Typography component="legend">Price</Typography>
+          <Typography component="legend">Preço</Typography>
           <Typography gutterBottom variant="subtitle1">
             {place.price_level}
           </Typography>
